4ef8ac82badbde966713d2a41ca35af7a9d12ae4
Paulina <paulina.paluch1@pollub.edu.pl>
2018-08-02 10:47:28 +0200

    Dodanie pobierania inputu od uzytkownika

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World! ");
+        int tab2[][]={{21,50,50},{22,90,90},{23,130,130},{24,220,260},{25,300,400},{26,400,600},{27,430,630},{28,460,660},
+                {29,490,690},{30,520,720},{31,700,720},{32,900,1350},{33,990,1440},{34,1250,1800},{36,1500,2200}};
+        int wartoscReki,etap;
+
+        System.out.println("Wartosc punktowa reki: ");
+        Scanner input=new Scanner(System.in);
+        wartoscReki=input.nextInt();
+
+        while(wartoscReki<21)
+        {
+            System.out.println("Wartosc nie moze byc mniejsza niz 21!");
+            wartoscReki=input.nextInt();
+        }
+
+        System.out.println("Przed partią - 1, Po partii - 2");
+        etap=input.nextInt();
+
+        while((etap!=1)&&(etap!=2))
+        {
+            System.out.println("Wybierz 1 lub 2!");
+            etap=input.nextInt();
+
+        }
+
+
 
 
 
