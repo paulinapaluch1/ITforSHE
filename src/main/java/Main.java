a1738ff0228991acf5f08f8806a96f6f208348f5
Paulina <paulina.paluch1@pollub.edu.pl>
2018-08-04 14:45:33 +0200

    utworzenie metody pobierajacej dane i zastapienie petli while instrukcja switch-case

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,47 @@
 import java.util.Scanner;
 
 public class Main {
-
-
+    private int tab2[][] = {{21, 50, 50}, {22, 90, 90}, {23, 130, 130}, {24, 220, 260}, {25, 300, 400}, {26, 400, 600}, {27, 430, 630}, {28, 460, 660},
+            {29, 490, 690}, {30, 520, 720}, {31, 700, 720}, {32, 900, 1350}, {33, 990, 1440}, {34, 1250, 1800}, {36, 1500, 2200}};
+    private int wartoscReki;
+    private String etap;
 
     public static void main(String[] args) {
 
-        int tab2[][]={{21,50,50},{22,90,90},{23,130,130},{24,220,260},{25,300,400},{26,400,600},{27,430,630},{28,460,660},
-                {29,490,690},{30,520,720},{31,700,720},{32,900,1350},{33,990,1440},{34,1250,1800},{36,1500,2200}};
-        int wartoscReki,etap;
+        Main obj = new Main();
+        obj.pobranieDanych(obj);
+
+
+
+    }
+
+    public void pobranieDanych(Main obj){
 
         System.out.println("Wartosc punktowa reki: ");
-        Scanner input=new Scanner(System.in);
-        wartoscReki=input.nextInt();
+        Scanner input = new Scanner(System.in);
+        obj.wartoscReki = input.nextInt();
 
-        while(wartoscReki<21)
-        {
+        while (obj.wartoscReki < 21) {
             System.out.println("Wartosc nie moze byc mniejsza niz 21!");
-            wartoscReki=input.nextInt();
-        }
-
-        System.out.println("Przed partią - 1, Po partii - 2");
-        etap=input.nextInt();
-
-        while((etap!=1)&&(etap!=2))
-        {
-            System.out.println("Wybierz 1 lub 2!");
-            etap=input.nextInt();
-
+            obj.wartoscReki = input.nextInt();
         }
 
 
+        System.out.println("Przed partią - wpisz 'przed', Po partii - wpisz 'po'");
+        obj.etap = input.next();
+
+        switch(obj.etap.toLowerCase()){
+
+            case "przed":
+            case "po":
+              obj.etap=etap.toLowerCase();
+                break;
+
+            default:
+                System.out.println("Przed partią - wpisz 'przed', Po partii - wpisz 'po'");
+                obj.etap = input.next();
+                break;
+        }
 
 
 
