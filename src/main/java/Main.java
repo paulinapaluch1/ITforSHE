9584e27e58164394080bec231bb3de565aa29c6a
Paulina <paulina.paluch1@pollub.edu.pl>
2018-08-06 10:02:02 +0200

    Dodanie przeliczania punktow wedlug ustalonych zasad

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     private int tab2[][] = {{21, 50, 50}, {22, 90, 90}, {23, 130, 130}, {24, 220, 260}, {25, 300, 400}, {26, 400, 600}, {27, 430, 630}, {28, 460, 660},
-            {29, 490, 690}, {30, 520, 720}, {31, 700, 720}, {32, 900, 1350}, {33, 990, 1440}, {34, 1250, 1800}, {36, 1500, 2200}};
+            {29, 490, 690}, {30, 520, 720}, {31, 700, 720}, {32, 900, 1350}, {33, 990, 1440}, {34, 1250, 1800}, {35,1400,2100},{36, 1500, 2200}};
     private int wartoscReki;
     private String etap;
 
@@ -38,4 +38,18 @@ public class Main {
 
 
     }
+
+
+    private int obliczWartoscReki(){
+
+        int j;
+        if(etap.toLowerCase().equals("przed")) j=1;
+        else j=2;
+
+        if(wartoscReki<=36) return tab2[wartoscReki-21][j];
+        else return tab2[15][j];
+
+    }
+
+
 }
