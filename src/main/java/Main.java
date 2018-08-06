6587c2e4fce7f418131bc24e2a0e25738575d864
Paulina <paulina.paluch1@pollub.edu.pl>
2018-08-06 21:04:35 +0200

    Pozbycie sie tzw. magicznych liczb

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,27 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     private int tab2[][] = {{21, 50, 50}, {22, 90, 90}, {23, 130, 130}, {24, 220, 260}, {25, 300, 400}, {26, 400, 600}, {27, 430, 630}, {28, 460, 660},
-            {29, 490, 690}, {30, 520, 720}, {31, 700, 720}, {32, 900, 1350}, {33, 990, 1440}, {34, 1250, 1800}, {35,1400,2100},{36, 1500, 2200}};
+            {29, 490, 690}, {30, 520, 720}, {31, 700, 720}, {32, 900, 1350}, {33, 990, 1440}, {34, 1250, 1800}, {35, 1400, 2100}, {36, 1500, 2200}};
     private int wartoscReki;
     private String etap;
+    private static final int  MIN_WARTOSC_REKI=21;
+    private static final int PRZELOMOWA_WARTOSC_REKI=36;
 
     public static void main(String[] args) {
 
         Main obj = new Main();
         obj.pobranieDanych();
-        System.out.println("Ta reka warta jest: "+obj.obliczWartoscReki());
-
+        System.out.println("Ta reka warta jest: " + obj.obliczWartoscReki());
 
     }
 
-    private void pobranieDanych(){
+    private void pobranieDanych() {
 
         System.out.println("Wartosc punktowa reki: ");
         Scanner input = new Scanner(System.in);
         wartoscReki = input.nextInt();
 
-        while (wartoscReki < 21) {
-            System.out.println("Wartosc nie moze byc mniejsza niz 21!");
+        while (wartoscReki < MIN_WARTOSC_REKI) {
+            System.out.println("Wartosc nie moze byc mniejsza niz "+MIN_WARTOSC_REKI+"!");
             wartoscReki = input.nextInt();
         }
 
@@ -36,18 +37,17 @@ public class Main {
         }
 
 
-
     }
 
 
-    private int obliczWartoscReki(){
+    private int obliczWartoscReki() {
 
         int j;
-        if(etap.toLowerCase().equals("przed")) j=1;
-        else j=2;
+        if (etap.toLowerCase().equals("przed")) j = 1;
+        else j = 2;
 
-        if(wartoscReki<=36) return tab2[wartoscReki-21][j];
-        else return tab2[15][j];
+        if (wartoscReki <= PRZELOMOWA_WARTOSC_REKI) return tab2[wartoscReki - MIN_WARTOSC_REKI][j];
+        else return tab2[tab2.length-1][j];
 
     }
 
