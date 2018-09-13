05e0f368b374aaa09efda76ff5c7aad0f7af404d
Paulina <paulina.paluch1@pollub.edu.pl>
2018-09-13 23:02:52 +0200

    Wprowadzenie plikow .csv

--- src/main/java/TableReader.java
+++ src/main/java/TableReader.java
@@ -2,25 +2,4 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class TableReader {
-
-
-    protected int[][] readTableFromFile(String path, int rows, int maxCol) {
-        int table[][] = new int[rows][maxCol];
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String line;
-            int row = 0;
-            while ((line = br.readLine()) != null) {
-                String[] valuesInLine = line.split(";");
-                for (int col = 0; col < maxCol; col++)
-                    table[row][col] = Integer.parseInt(valuesInLine[col]);
-                row++;
-            }
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-
-        return table;
-    }
-}
 
