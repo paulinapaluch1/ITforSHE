23ed69c12d51e34b2c8ab01ecdac51267952d6e5
Paulina <paulina.paluch1@pollub.edu.pl>
2018-09-25 11:02:01 +0200

    Utworzenie katalogu site i apt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -17,6 +16,17 @@ public class Main  extends Application {
     private static final int LAST_ROW_IN_IMP_TABLE = 23;
     private static final int MINIMAL_RANGE_OFFSET = 1;
     private static final int MAXIMUM_RANGE_OFFSET = 2;
+    private static final int ROWS_IN_IMP_TABLE = 24;
+    private static final int COLS_IN_INP_TABLE = 3;
+    private static final int ROWS_IN_HAND_VALUES_TABLE = 16;
+    private static final int COLS_IN_HAND_VALUE_TABLE = 3;
+    private static final int ROWS_IN_GAMES_TABLES = 8;
+    private static final int COLS_IN_GAMES_TABLES = 13;
+    private static final String HAND_VALUES_FILE_PATH="src/main/resources/HandValues.csv";
+    private static final String IMP_VALUES_FILE_PATH="src/main/resources/IMPValues.csv";
+    private static final String GAME_VALUES_BEFORE_MATH_FILE_PATH="src/main/resources/GameValuesBeforeMatch.csv";
+    private static final String GAME_VALUES_AFTER_MATH_FILE_PATH="src/main/resources/GameValuesAfterMatch.csv";
+
     private int handValue;
     private int amountOfBidedTricks;
     private int amountOfBloopers;
@@ -26,38 +36,11 @@ public class Main  extends Application {
     private Suit suit;
     private char veto;
     private char reveto = 'n';
-
-    private int tableOFPossibleHandValuesBeforeAndAfterMatch[][] = {{21, 50, 50}, {22, 90, 90}, {23, 130, 130}, {24, 220, 260},
-            {25, 300, 400}, {26, 400, 600}, {27, 430, 630}, {28, 460, 660},
-            {29, 490, 690}, {30, 520, 720}, {31, 700, 720}, {32, 900, 1350},
-            {33, 990, 1440}, {34, 1250, 1800}, {35, 1400, 2100}, {36, 1500, 2200}};
-
-    private int tableOfGameValuesBeforeMatch[][] = {{1, 50, 100, 200, 70, 140, 230, 80, 160, 520, 90, 180, 560},
-            {2, 100, 300, 600, 90, 180, 560, 110, 470, 640, 120, 490, 680},
-            {3, 150, 500, 1000, 110, 470, 640, 140, 530, 760, 400, 550, 800},
-            {4, 200, 800, 1600, 130, 510, 720, 420, 590, 880, 430, 610, 920},
-            {5, 250, 1100, 2200, 400, 550, 800, 450, 650, 1000, 460, 670, 1040},
-            {6, 300, 1400, 2800, 920, 1090, 1380, 980, 1210, 1620, 990, 1230, 1660},
-            {7, 350, 1700, 3400, 1440, 1630, 1960, 1510, 1770, 2240, 1520, 1790, 2280},
-            {0, 0, 0, 0, 20, 100, 200, 30, 100, 200, 30, 100, 200}};
-
-    private int tableOfGameValuesAfterMatch[][] = {{1, 100, 200, 400, 70, 140, 230, 80, 160, 720, 90, 180, 760},
-            {2, 200, 500, 1000, 90, 180, 760, 110, 670, 840, 120, 690, 880},
-            {3, 300, 800, 1600, 110, 670, 840, 140, 730, 960, 600, 750, 1000},
-            {4, 400, 1100, 2200, 130, 710, 920, 620, 790, 1080, 630, 810, 1120},
-            {5, 600, 1400, 2800, 600, 750, 1000, 650, 850, 1200, 660, 870, 1240},
-            {6, 600, 1700, 3400, 1370, 1510, 1830, 1430, 1660, 2070, 1440, 1680, 2110},
-            {7, 700, 2000, 4000, 2140, 2330, 2660, 2210, 2470, 2290, 2200, 2490, 2980},
-            {0, 0, 0, 0, 20, 100, 200, 30, 400, 400, 30, 200, 400}
-
-    };
-
-    private int tableOfIMP[][] = {{20, 40, 1}, {50, 80, 2}, {90, 120, 3}, {130, 160, 4}, {170, 210, 5},
-            {220, 260, 6}, {270, 310, 7}, {320, 360, 8}, {370, 420, 9}, {430, 490, 10}, {500, 590, 11},
-            {600, 740, 12}, {750, 890, 13}, {900, 1090, 14}, {1100, 1290, 15}, {1300, 1490, 16},
-            {1500, 1740, 17}, {1750, 1990, 18}, {2000, 2240, 19}, {2250, 2490, 20}, {2500, 2990, 21},
-            {3000, 3490, 22}, {3500, 3990, 23}, {0, 4000, 24}};
-
+    TableReader reader;
+    private int tableOFPossibleHandValuesBeforeAndAfterMatch[][];
+    private int tableOfGameValuesBeforeMatch[][];
+    private int tableOfGameValuesAfterMatch[][];
+    private int tableOfIMP[][];
 
 
     @Override
@@ -75,6 +58,11 @@ public class Main  extends Application {
         int matchScore;
         int scoreIMP;
         Main obj = new Main();
+        obj.reader = new TableReader();
+        obj.tableOFPossibleHandValuesBeforeAndAfterMatch = obj.reader.readTableFromFile(HAND_VALUES_FILE_PATH, ROWS_IN_HAND_VALUES_TABLE, COLS_IN_HAND_VALUE_TABLE);
+        obj.tableOfIMP = obj.reader.readTableFromFile(IMP_VALUES_FILE_PATH, ROWS_IN_IMP_TABLE, COLS_IN_INP_TABLE);
+        obj.tableOfGameValuesBeforeMatch = obj.reader.readTableFromFile(GAME_VALUES_BEFORE_MATH_FILE_PATH, ROWS_IN_GAMES_TABLES, COLS_IN_GAMES_TABLES);
+        obj.tableOfGameValuesAfterMatch = obj.reader.readTableFromFile(GAME_VALUES_AFTER_MATH_FILE_PATH, ROWS_IN_GAMES_TABLES, COLS_IN_GAMES_TABLES);
         try {
             obj.getHandValueAndStage();
             valueOfHand = obj.calculateValueOfHand();
@@ -88,8 +76,6 @@ public class Main  extends Application {
                 | InvalidAmountOfTricksException | ImproperCharacterException e) {
             System.err.println(e.getMessage());
         }
-
-
 
     }
 
---
+++ src/main/java/TableReader.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TableReader {
+
+
+    protected int[][] readTableFromFile(String path, int maxRow, int maxCol) {
+        int table[][] = new int[maxRow][maxCol];
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            int row = 0;
+            while ((line = br.readLine()) != null) {
+                String[] valuesInLine = line.split(";");
+                for (int col = 0; col < maxCol; col++)
+                    table[row][col] = Integer.parseInt(valuesInLine[col]);
+                row++;
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return table;
+    }
+}
+
